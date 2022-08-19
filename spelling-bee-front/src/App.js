fa89f377fb165865042627775ccc6a1348d0f146
Tarcisio <35512873+tcortes55@users.noreply.github.com>
2022-08-19 16:47:40 -0300

    Hive cell hexagon

--- spelling-bee-front/src/App.js
+++ spelling-bee-front/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <HiveCell></HiveCell>
+        <HiveCell>A</HiveCell>
         <a
           className="App-link"
           href="https://reactjs.org"
--- spelling-bee-front/src/components/hiveCell/hiveCell.js
+++ spelling-bee-front/src/components/hiveCell/hiveCell.js
@@ -3,38 +3,50 @@ import styled, { css } from 'styled-components';
 
 const Hexagon = styled.div`
     margin-top: 30px;
-    width: 104px;
-    height: 60px;
+    height: 104px;
+    width: 60px;
     background-color: #6C6;
     position: relative;
 
     &:before {
         content: " ";
         width: 0; height: 0;
-        border-bottom: 30px solid #6C6;
-        border-left: 52px solid transparent;
-        border-right: 52px solid transparent;
+        border-right: 30px solid #6C6;
+        border-bottom: 52px solid transparent;
+        border-top: 52px solid transparent;
         position: absolute;
-        top: -30px;
-        left: 0px;
+        top: 0px;
+        left: -30px;
     }
 
     &:after {
         content: "";
         width: 0;
         position: absolute;
-        bottom: -30px;
-        left: 0px;
-        border-top: 30px solid #6C6;
-        border-left: 52px solid transparent;
-        border-right: 52px solid transparent;
+        right: -30px;
+        top: 0px;
+        border-left: 30px solid #6C6;
+        border-bottom: 52px solid transparent;
+        border-top: 52px solid transparent;
     }
 `;
 
-function HiveCell() {
+const HexagonContet = styled.div`
+    position: relative;
+    top: 50%;
+    -webkit-transform: translateY(-50%);
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
+`;
+
+function HiveCell({ children }) {
     return (
         <div>
-            <Hexagon>A</Hexagon>
+            <Hexagon>
+                <HexagonContet>
+                    { children }
+                </HexagonContet>
+            </Hexagon>
         </div>
     )
 }
