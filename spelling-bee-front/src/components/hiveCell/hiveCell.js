5c509bce6a04831680244cb8f8c03175bbe01f84
Tarcisio <35512873+tcortes55@users.noreply.github.com>
2022-08-19 00:01:03 -0300

    Hexagon component (wip)

--- spelling-bee-front/src/components/hiveCell/hiveCell.js
+++ spelling-bee-front/src/components/hiveCell/hiveCell.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+
+const Hexagon = styled.div`
+    margin-top: 30px;
+    width: 104px;
+    height: 60px;
+    background-color: #6C6;
+    position: relative;
+
+    &:before {
+        content: " ";
+        width: 0; height: 0;
+        border-bottom: 30px solid #6C6;
+        border-left: 52px solid transparent;
+        border-right: 52px solid transparent;
+        position: absolute;
+        top: -30px;
+    }
+
+    &:after {
+        content: "";
+        width: 0;
+        position: absolute;
+        bottom: -30px;
+        border-top: 30px solid #6C6;
+        border-left: 52px solid transparent;
+        border-right: 52px solid transparent;
+    }
+`;
 
 function HiveCell() {
     return (
         <div>
+            <Hexagon></Hexagon>
             TEST
         </div>
     )
