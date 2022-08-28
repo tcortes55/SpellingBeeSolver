4196c366103d956ead8c936ccb623e20d9dbd75f
Tarcisio <35512873+tcortes55@users.noreply.github.com>
2022-08-28 19:55:05 -0300

    Collapse other panels on change

--- spelling-bee-front/src/components/panel/panel.js
+++ spelling-bee-front/src/components/panel/panel.js
@@ -12,24 +12,31 @@ const AccordionContainer = styled.div`
     width: 280px;
 `;
 
-function Panel({ gameResponse, settings, children }) {
-    
+function Panel({ gameResponse, settings, currentPanel, handleChange, children }) {
+    let isExpanded = settings.Title === currentPanel;
+
     return (
         <>
-            <Accordion defaultExpanded={settings.DefaultExpanded}>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                >
-                    {settings.Title}
-                </AccordionSummary>
-                <AccordionDetails>
-                    <AccordionContainer>
-                        <Hive responseLetters={gameResponse.letters ? gameResponse.letters : Strings.EmptyHive}></Hive>
-                        <AnswerBox words={gameResponse.words ? gameResponse.words : []}></AnswerBox>
-                        {children}
-                    </AccordionContainer>
-                </AccordionDetails>
+            <Accordion
+                defaultExpanded={settings.DefaultExpanded}
+                expanded={isExpanded}
+                onChange={handleChange(settings.Title)}
+            >
+                    <AccordionSummary
+                        expandIcon={
+                            isExpanded ? <div></div> : <ExpandMoreIcon /> 
+                        }
+                        aria-controls="panel1a-content"
+                    >
+                        {settings.Title}
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <AccordionContainer>
+                            <Hive responseLetters={gameResponse.letters ? gameResponse.letters : Strings.EmptyHive}></Hive>
+                            <AnswerBox words={gameResponse.words ? gameResponse.words : []}></AnswerBox>
+                            {children}
+                        </AccordionContainer>
+                    </AccordionDetails>
             </Accordion>
         </>
     );
