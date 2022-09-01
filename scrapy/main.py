ad88229c01cc66adf5f3cb04996a6792d3a225e3
Tarcisio <35512873+tcortes55@users.noreply.github.com>
2022-08-31 21:43:47 -0300

    API with Flask (wip)

---
+++ scrapy/main.py
@@ -0,0 +1,45 @@
+import crochet
+crochet.setup()
+
+from flask import Flask , render_template, jsonify, request, redirect, url_for
+from scrapy import signals
+from scrapy.crawler import CrawlerRunner
+from scrapy.signalmanager import dispatcher
+import time
+
+from spellingbeenyt.spellingbeenyt.spiders.spellingbeenyt import SpellingbeespiderSpider
+
+
+# Creating Flask App Variable
+
+app = Flask(__name__)
+
+output_data = []
+crawl_runner = CrawlerRunner()
+
+@app.route("/scrape")
+def scrape():
+
+    baseURL = "https://www.nytimes.com/puzzles/spelling-bee"
+    scrape_with_crochet(baseURL=baseURL) # Passing that URL to our Scraping Function
+
+    time.sleep(5) # Pause the function while the scrapy spider is running
+    
+    return jsonify(output_data) # Returns the scraped data after being running for 20 seconds.
+
+@crochet.run_in_reactor
+def scrape_with_crochet(baseURL):
+    # This will connect to the dispatcher that will kind of loop the code between these two functions.
+    dispatcher.connect(_crawler_result, signal=signals.item_scraped)
+    
+    # This will connect to the ReviewspiderSpider function in our scrapy file and after each yield will pass to the crawler_result function.
+    eventual = crawl_runner.crawl(SpellingbeespiderSpider, category = baseURL)
+    return eventual
+
+#This will append the data to the output data list.
+def _crawler_result(item, response, spider):
+    output_data.append(dict(item))
+
+
+if __name__== "__main__":
+    app.run(debug=True)
